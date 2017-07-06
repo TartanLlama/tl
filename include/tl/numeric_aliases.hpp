456bf9998c551b7aeafd661c91f5b19cf2ab9b99
Simon Brand <tartanllama@gmail.com>
2017-07-06 18:59:33 +0100

    Add numeric aliases

---
+++ include/tl/numeric_aliases.hpp
@@ -0,0 +1,24 @@
+#include <cstdint>
+
+namespace tl {
+    using i8 = std::int8_t;
+    using i16 = std::int16_t;
+    using i32 = std::int32_t;
+    using i64 = std::int64_t;
+
+    using u8 = std::uint8_t;
+    using u16 = std::uint16_t;
+    using u32 = std::uint32_t;
+    using u64 = std::uint64_t;
+
+    using uchar = unsigned char;
+    using ushort = unsigned short;
+    using uint = unsigned int;
+    using ulong = unsigned long;
+    using ullong = unsigned long long;
+
+    using llong = long long;
+    using ldouble = long double;
+
+    using usize = std::size_t;
+}
