5d95e0a7db1fb061550d0dda57d484b26a73793b
Simon Brand <tartanllama@gmail.com>
2017-07-06 19:49:09 +0100

    Update dependent false

--- test/dependent_false.cpp
+++ test/dependent_false.cpp
@@ -1,12 +1,12 @@
 #include <type_traits>
 #include "tl/dependent_false.hpp"
 
-template <class T, std::enable_if_t<std::is_integral<T>::value>* = nullptr>
+template <class T, typename std::enable_if<std::is_integral<T>::value>::type* = nullptr>
 void foo(T t){}
 
-template <class T, std::enable_if_t<!std::is_integral<T>::value>* = nullptr>
+template <class T, typename std::enable_if<!std::is_integral<T>::value>::type* = nullptr>
 void foo(T t) {
-    static_assert(tl::dependent_false<T>::value, "Not integral");
+    static_assert(dependent_false<T>::value, "Not integral");
 }
 
 int main() {
