e24a47385472f272ea95ad98c39323d896fb4b38
Simon Brand <tartanllama@gmail.com>
2017-07-06 19:51:49 +0100

    Update dependent false

--- test/dependent_false.cpp
+++ test/dependent_false.cpp
@@ -6,7 +6,7 @@ void foo(T t){}
 
 template <class T, typename std::enable_if<!std::is_integral<T>::value>::type* = nullptr>
 void foo(T t) {
-    static_assert(dependent_false<T>::value, "Not integral");
+    static_assert(tl::dependent_false<T>::value, "Not integral");
 }
 
 int main() {
