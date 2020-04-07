06d7fc38010f98492432548f998759e855208da0
ericLemanissier <ericLemanissier@users.noreply.github.com>
2020-04-07 09:58:05 +0200

    add test for overload
    this test fails, because dispatcher actually calls the int overload

--- test/overload.cpp
+++ test/overload.cpp
@@ -4,9 +4,11 @@
 int main() {
     auto dispatcher = tl::overload(
         [](int i) { return 42; },
-        [](char const* f) { return 24; }
+        [](char const* f) { return 24; },
+        [](bool b) { return 66; }
     );
 
     assert(dispatcher(0) == 42);
     assert(dispatcher("hi") == 24);
+    assert(dispatcher(true) == 66);
 }
